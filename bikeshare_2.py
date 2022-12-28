14e21509d329cb6487584b55a7817ba7dd30511d
HungTa <hungtd1709@gmail.com>
2022-12-28 10:16:39 +0700

    refactor: Add print to station_stats function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -84,6 +84,8 @@ def station_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+    
+    print('\nstation_stats function\n')
 
 
 def trip_duration_stats(df):
